3a2a7a074058c8e8e34a46f75ba3ccd7b158f8f0
anonymous anonymous <biruk.bekele@kellogg.northwestern.edu>
2022-03-09 22:17:56 +0000

    Whitelist columns in Budget for ActiveAdmin

--- app/admin/budgets.rb
+++ app/admin/budgets.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Budget do
 
+ permit_params :user_id, :category_id, :amount
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
