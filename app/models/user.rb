138b821e6502137a504ce23d546a17b421377dbc
anonymous anonymous <biruk.bekele@kellogg.northwestern.edu>
2022-03-09 22:17:39 +0000

    Direct association between users and budgets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :budgets,
+             :dependent => :destroy
+
   has_many   :transactions,
              :dependent => :destroy
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:budgets) }
+
     it { should have_many(:transactions) }
 
     end
