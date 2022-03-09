bf2a4083274c36a2df07e15454e8b3acb5423461
anonymous anonymous <biruk.bekele@kellogg.northwestern.edu>
2022-03-09 22:17:52 +0000

    Whitelist columns in Account for ActiveAdmin

--- app/admin/accounts.rb
+++ app/admin/accounts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Account do
 
+ permit_params :account_type, :balance
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
