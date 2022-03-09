4ade95a5c014fe168818429040bca50006415a99
anonymous anonymous <biruk.bekele@kellogg.northwestern.edu>
2022-03-09 22:17:47 +0000

    Whitelist columns in Transaction for ActiveAdmin

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Transaction do
 
+ permit_params :user_id, :amount, :category_id, :transaction_date, :account_id, :event
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
